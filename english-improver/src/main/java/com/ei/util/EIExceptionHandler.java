cb6e07d90a05ea19dc7b886d4559e923cfed42fc
SongRunqi <yitiansong4@gmail.com>
2023-12-17 22:35:14 +0800

    add Result and Jwt utils

--- english-improver/src/main/java/com/ei/util/EIExceptionHandler.java
+++ english-improver/src/main/java/com/ei/util/EIExceptionHandler.java
@@ -10,11 +10,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author 86157
  * @date 2023/12/16
  */
-@ControllerAdvice
+
 public class EIExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
+
     public Result handleException(Exception e) {
         return Result.fail(e.getMessage());
     }
---
+++ english-improver/src/main/java/com/ei/util/JwtUtil.java
@@ -0,0 +1,68 @@
+package com.ei.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.ei.entities.ImproverUsers;
+
+import java.util.Date;
+
+/**
+ * @author 86157
+ * @date 2023/12/17
+ */
+
+public class JwtUtil {
+    private final String secret = "english-improver"; // 使用随机秘钥
+
+    /**
+     * 生成jwt token
+     * @param user 用户
+     * @return token
+     */
+    public String generateToken(ImproverUsers user) {
+        long nowMills = System.currentTimeMillis();
+        Date now = new Date(nowMills);
+        // 过期时间
+        long expire = 3600L;
+        Date expireDate = new Date(nowMills + expire * 1000);
+        return JWT.create()
+                .withClaim("username", user.getUsername())
+                .withClaim("id", user.getId())
+                .withIssuedAt(now)
+                .withExpiresAt(expireDate)
+                .sign(com.auth0.jwt.algorithms.Algorithm.HMAC256(secret));
+    }
+
+    /**
+     * 从token中获取用户id
+     * @param token token
+     * @return 用户id
+     */
+    public String getUserIdFromToken(String token) {
+        return JWT.decode(token).getClaim("id").asString();
+    }
+    /**
+     * 判断token是否已经失效
+     * @param jwt jwt
+     * @return 是否已经失效
+     */
+    private boolean isTokenExpired(DecodedJWT jwt) {
+        return jwt.getExpiresAt().before(new Date());
+    }
+    /**
+     * 校验token是否正确
+     * @param token token
+     * @return 是否正确
+     */
+    public boolean validateToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(secret); // 使用相同的密钥
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("auth0")
+                .build(); // 创建验证器
+        DecodedJWT jwt = verifier.verify(token); // 验证令牌
+        return !isTokenExpired(jwt); // 判断是否已经失效
+    }
+
+}
